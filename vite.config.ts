ca57ee1287d6bee51b806f6b072b3d8a0955d244
JacobGirling98 <jacob.girling98@gmail.com>
2024-04-27 09:23:04 +0100

    testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./test/setup.ts",
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
 })
