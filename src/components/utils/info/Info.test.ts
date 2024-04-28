a1bc10b5b5a81b8c2de49eb17b846f80bec6277b
Mona Lisa <jacob.girling98@gmail.com>
2024-04-28 14:34:29 +0100

    start of test

---
+++ src/components/utils/info/Info.test.ts
@@ -0,0 +1,21 @@
+import React from "react"
+import { render, screen, userEvent } from "@testing-library/react"
+import Info from "./Info"
+
+describe("Info component", () => {})
+
+// describe('Info component', () => {
+//   it('should render InfoContent component when button is clicked', async () => {
+//     const mockDate = '2024-04-28';
+//     render(<Info date={mockDate} />);
+
+//     // Find the button
+//     const button = screen.getByRole('button');
+
+//     // Simulate button click
+//     userEvent.click(button);
+
+//     // Ensure InfoContent is rendered
+//     await expect(screen.getByText(`App deployed at: ${mockDate}`)).toBeInTheDocument();
+//   });
+// });
